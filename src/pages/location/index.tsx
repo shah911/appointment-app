dfbecd5c4a1a9fef6bcc61455eff44b781ac68d8
shah 911 <mianshahzeb911@gmail.com>
2024-06-25 01:17:54 +0500

    added a location page

---
+++ src/pages/location/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+function Location() {
+  return (
+    <>
+      <div className="w-full bg-black relative">
+        <Navbar />
+      </div>
+      <div className="mx-auto w-[90%] my-10 2xl:my-[3vw]">Location</div>
+    </>
+  );
+}
+
+export default Location;
